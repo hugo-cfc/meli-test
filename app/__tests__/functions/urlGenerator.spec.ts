5a5e426b14f6d773aee067aed6123ce7a139b3ae
Hugo Cesar Firmino Costa <hugocesar@MacBook-Air-de-Hugo.local>
2023-06-20 19:16:23 -0300

    test: function: urlGenerator tests

---
+++ app/__tests__/functions/urlGenerator.spec.ts
@@ -0,0 +1,46 @@
+import urlGenerator from "../../src/functions/urlGenerator";
+
+describe("urlGenerator", () => {
+  it("should generate the correct URL without options", () => {
+    const config = {
+      pathname: "/example",
+      api: true,
+    };
+
+    const url = urlGenerator(config);
+
+    expect(url).toBe("?q=/example");
+  });
+
+  it("should generate the correct URL with options", () => {
+    const config = {
+      pathname: "/example",
+      api: true,
+    };
+
+    const options = {
+      sort: "name",
+      price: "asc",
+    };
+
+    const url = urlGenerator(config, options);
+
+    expect(url).toBe("?q=/example&sort=name&price=asc");
+  });
+
+  it("should generate the correct URL with null options", () => {
+    const config = {
+      pathname: "/example",
+      api: false,
+    };
+
+    const options = {
+      sort: null,
+      price: null,
+    };
+
+    const url = urlGenerator(config, options);
+
+    expect(url).toBe("/example");
+  });
+});
