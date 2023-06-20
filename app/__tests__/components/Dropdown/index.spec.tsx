5b62b74bb7bf34929fdd34be50ca332202e0c23d
Hugo Cesar Firmino Costa <hugocesar@MacBook-Air-de-Hugo.local>
2023-06-20 02:21:28 -0300

    test: dropdown test

---
+++ app/__tests__/components/Dropdown/index.spec.tsx
@@ -0,0 +1,69 @@
+import { fireEvent, render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+
+import { setIsSorterDropdownOpen } from "../../../src/app/redux/Features/productsSlice";
+import Dropdown from "../../../src/Components/Dropdown";
+
+const mockStore = configureStore([]);
+
+describe("<Dropdown />", () => {
+  it("should render when isSorterDropdownOpen is true", () => {
+    const initialState = {
+      products: {
+        isSorterDropdownOpen: true,
+      },
+    };
+
+    const store = mockStore(initialState);
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <Dropdown>Dropdown Content</Dropdown>
+      </Provider>
+    );
+
+    expect(getByText("Dropdown Content")).toBeInTheDocument();
+  });
+
+  it("shouldn't render when isSorterDropdownOpen is false", () => {
+    const initialState = {
+      products: {
+        isSorterDropdownOpen: false,
+      },
+    };
+
+    const store = mockStore(initialState);
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <Dropdown>Dropdown Content</Dropdown>
+      </Provider>
+    );
+
+    expect(getByText("Dropdown Content")).toBeInTheDocument();
+  });
+
+  it("calls dispatch correctly when clicking on div overlay", () => {
+    const initialState = {
+      products: {
+        isSorterDropdownOpen: true,
+      },
+    };
+    const store = mockStore(initialState);
+    store.dispatch = jest.fn();
+
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <Dropdown>Conteúdo do Dropdown</Dropdown>
+      </Provider>
+    );
+
+    const overlayDiv = getByTestId("overlay-div");
+    fireEvent.click(overlayDiv);
+
+    store.dispatch(setIsSorterDropdownOpen(false));
+
+    expect(store.dispatch).toHaveBeenCalledWith(setIsSorterDropdownOpen(false));
+  });
+});
