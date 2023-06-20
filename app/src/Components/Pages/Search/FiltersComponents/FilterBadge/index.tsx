5c57cafa98a0792212518421b419a95d415112cf
Hugo Cesar Firmino Costa <hugocesar@MacBook-Air-de-Hugo.local>
2023-06-20 06:42:29 -0300

    test: filterBadge tests is ready

--- app/src/Components/Pages/Search/FiltersComponents/FilterBadge/index.tsx
+++ app/src/Components/Pages/Search/FiltersComponents/FilterBadge/index.tsx
@@ -14,7 +14,7 @@ const FilterBadge = ({ title }: FilterBadgeProps) => {
   return (
     <button
       className="flex w-fit items-center gap-x-1 rounded-3xl bg-gray-300 px-2 py-1 text-sm font-light text-grayTextML tablet:rounded-none tablet:bg-white tablet:py-0 tablet:text-xs"
-      onClick={() => handleRemoveFilter()}
+      onClick={handleRemoveFilter}
     >
       {title}
       <XIcon className="w-4 text-gray-600 tablet:text-grayML" />
