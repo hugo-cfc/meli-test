39fbba04943d62e56184e7ce403831b7d9b463bd
Hugo Cesar Firmino Costa <hugocesar@MacBook-Air-de-Hugo.local>
2023-06-22 09:52:01 -0300

    test: adding missing tests on DropdownItem

--- app/__tests__/components/Pages/Search/SorterComponents/DropdownItem/index.spec.tsx
+++ app/__tests__/components/Pages/Search/SorterComponents/DropdownItem/index.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { useSearchParams } from "next/navigation";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
@@ -20,6 +20,14 @@ jest.mock("next/navigation", () => ({
   useRouter: jest.fn(),
   useSearchParams: jest.fn(),
 }));
+
+const handleClickOnSortOptionMock = jest.fn();
+
+jest.mock("../../../../../../src/hooks/useSorters", () => {
+  return jest.fn(() => ({
+    handleClickOnSortOption: handleClickOnSortOptionMock,
+  }));
+});
 
 describe("<DropdownItem />", () => {
   it("should render correctly and with active indicator", () => {
@@ -59,5 +67,19 @@ describe("<DropdownItem />", () => {
 
     expect(button).toHaveClass("text-grayTextML");
     expect(activeIndicator).toBeNull();
+  });
+
+  it("should call handleClickOnFilterOption function when filter is clicked", () => {
+    render(
+      <Provider store={store}>
+        <DropdownItem item={sortMock[1]} />
+      </Provider>
+    );
+
+    const button = screen.getByText(sortMock[1].name);
+
+    fireEvent.click(button);
+
+    expect(handleClickOnSortOptionMock).toHaveBeenCalledWith(sortMock[1].id);
   });
 });
